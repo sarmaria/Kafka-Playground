190be03dfa24ed8c85aec14e4e0c42a9937c504b
charantechie <charantechie@gmail.com>
2026-06-15 16:38:17 +0200

    Simple Kafka Consumer

---
+++ src/main/java/com/broker/kafkaspringboot/section1/Section1Runner.java
@@ -0,0 +1,16 @@
+package com.broker.kafkaspringboot.section1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section1Runner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section1Runner.class,
+                "--section=section1",
+                "--config=01-simple-consumer");
+    }
+
+}
+
